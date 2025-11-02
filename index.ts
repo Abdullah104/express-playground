163d98c6c7ab3b3779a34a34554b264847aa83c3
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2025-11-02 17:15:15 +0300

    Basic routing

--- index.ts
+++ index.ts
@@ -7,6 +7,18 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.post("/", (req, res) => {
+  res.send("post!");
+});
+
+app.put("/user", (req, res) => {
+  res.send("putting user");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("deleting user");
+});
+
 app.listen(port, () =>
-  console.log(`Express is listening at http://localhost:${port}`)
+  console.log(` Express is listening at http://localhost:${port}`)
 );
